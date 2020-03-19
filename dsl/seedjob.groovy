52ada9483f475e18bff5f1db0f08474ecd0d628d
aafreen <aafreen@usa-mac-nis1713.attlocal.net>
2020-03-18 17:42:39 -0700

    seedjob.groovy

--- dsl/seedjob.groovy
+++ dsl/seedjob.groovy
@@ -19,35 +19,12 @@ def createPackageJob(jobName, repoUrl) {
     }
 }
 
-def createTestJob(jobName, repoUrl) {
-    pipelineJob(jobName) {
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        remote {
-                            url(repoUrl)
-                        }
-                        branches('master')
-                        extensions {
-                            cleanBeforeCheckout()
-                    }
-                }
-                scriptPath("Jenkinsfile")
-            }
-        }
-    }
-
-  }
-}
 
 def buildPipelineJobs() {
     def repoUrl = "https://github.com/amenaafreen/springbootwebapp.git"
     def packageName = "job_package"
-    def testName = "job_test"
 
     createPackageJob(packageName, repoUrl)
-    createTestJob(testName, repoUrl)
 }
 
 buildPipelineJobs()
