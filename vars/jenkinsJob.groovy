f9667362b4daa01abb320f0ae499f2bfbcd29c9f
aafreen <aafreen@usa-mac-nis1713.attlocal.net>
2020-03-18 17:54:11 -0700

    jenkinsjob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -3,21 +3,11 @@ def call(){
         stage('Checkout') {
             checkout scm
         }
-
-        // Execute different stages depending on the job
-        if(env.JOB_NAME.contains("package")){
-            packageArtifact()
-        }
-        }
-}
-
-
-def packageArtifact(){
-    stage("Package artifact") {
+        stage('Package artifact') {
         def mvn_version = 'Maven'
         withEnv( ["PATH+MAVEN=${tool mvn_version}/bin"] ) {
-        sh "mvn -DskipTests clean package"
+        sh "mvn clean package"
     }
+   }    
+ }  
 }
-}
-
